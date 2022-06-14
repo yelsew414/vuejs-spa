f9da92a62e8b86805cfe3635eb63bf9bb60cb3d9
yelsew414 <yelsew414@gmail.com>
2022-06-14 15:27:24 -0600

    added d3

---
+++ src/views/BarchartView.vue
@@ -0,0 +1,23 @@
+<script>
+import * as d3 from 'd3'
+export default {
+    data() {
+        return {
+            greeting: 'Hello World!'
+    }
+  }
+}
+    console.log(d3)
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+  <svg></svg>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
