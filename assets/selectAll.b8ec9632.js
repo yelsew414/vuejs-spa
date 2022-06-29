dec54404b59fae5c067c50f75b39eb5db17f74a1
= <=>
2022-06-29 15:18:13 -0600

    deploy

---
+++ assets/selectAll.b8ec9632.js
@@ -0,0 +1,1 @@
+import{S as t,g as n,r as o}from"./index.53fb2320.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([n(e)],o)}export{a as s};
