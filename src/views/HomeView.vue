5d59cfe990ae393fc7bd6b792d06526c89cf0ead
= <=>
2022-06-29 16:17:00 -0600

    Updated homeview to have info about website purpose

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,16 @@ import TheWelcome from '@/components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">Welcome</h1>
+   <div class="box">
+      <div class="block">Hi, welcome to my D3.js demo site! Select a type of chart from the menu above. </div>
+      <div class="block">
+        I made these charts as a proof of concept for different D3.js vizualizations includig interactivity and animation. 
+      </div>
+   </div> 
+   <h1 class="title">Learn more about D3.js</h1>
+   <div class="box">
+    I hope to use this site to display more D3 vizualizations including ongoing collaborative projects. For more inspiration I put together some of my favorite online D3 resources <a href="https://github.com/wesleysmith1/d3-resources">here</a>. In particular, check out the D3-gallary under Examples.
+   </div>
   </main>
 </template>
